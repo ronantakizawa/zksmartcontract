272b29e772fcbd1908011ad4e6d5b4f3aa7e18d7
Ronan Takizawa <71115970+ronantakizawa@users.noreply.github.com>
2024-02-16 17:11:37 -0700

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
-app.use(express.static(path.join(__dirname, '.'))); // Serve static files from the current directory
+app.use(express.static(path.join(__dirname, 'js'))); // Serve static files from the current directory
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html')); // Replace 'yourHtmlFileName.html' with the name of your HTML file
